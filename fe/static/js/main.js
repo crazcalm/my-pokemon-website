24b8d659d995e8785393c22042b3d1d3b1932e87
Marcus Willock <crazcalm@gmail.com>
2018-09-08 12:22:39 -0400

    Add form toggle via radio buttons

--- fe/static/js/main.js
+++ fe/static/js/main.js
@@ -1,1 +1,52 @@
-console.log("hello world");
+var app = {
+	init : function(){
+		this.callbacks.hideForms();
+
+		//radio button form selector callbacks
+		var ids = document.querySelectorAll(app.dom.forms.radioSelectorID);
+		for (var i=0; i<ids.length; i++){
+			ids[i].addEventListener("change", app.callbacks.showAForm);
+		}
+	},
+
+	callbacks : {
+		hideForms : function(){
+			app.dom.forms.hideAll();
+		},
+		showAForm : function(){
+			var ids = document.querySelectorAll(app.dom.forms.radioSelectorID);
+			for (var i = 0; i<ids.length; i++){
+				app.dom.forms.showAForm(ids[i]);
+			}
+		},
+	},
+
+	dom : {
+
+	  forms: {
+	  	showAForm : function(box){
+			if (box.checked){
+				document.querySelector(box.value).style.display = "";
+			}else{
+				document.querySelector(box.value).style.display = "none";
+			}
+	  	},
+		radioSelectorID : ".form_selector",
+
+	    ids : ["#energy_form", "#pokemon_form", "#trainer_form"],
+		hideAll : function(){
+			this.ids.forEach(function(id){
+				document.querySelector(id).style.display = "none";
+			});
+		},
+		showAll : function(){
+			this.ids.forEach(function(id){
+				document.querySelector(id).style.display = "";
+			});
+		},
+	  },
+		
+	},
+};
+
+window.onload = function(){ app.init();};
