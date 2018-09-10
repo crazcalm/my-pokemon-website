86d854ae4438dc703b86de98f01895f57ee4b233
Marcus Willock <crazcalm@gmail.com>
2018-09-10 01:07:41 -0400

    WIP: Add label to forms fields and add a callback stub to submit buttons

--- fe/static/js/main.js
+++ fe/static/js/main.js
@@ -7,6 +7,12 @@ var app = {
 		for (var i=0; i<ids.length; i++){
 			ids[i].addEventListener("change", app.callbacks.showAForm);
 		}
+
+		//Submit button form selector callbacks
+		var submitButtons = document.querySelectorAll(app.dom.forms.submitButtons);
+		for (i=0; i<submitButtons.length; i++){
+			submitButtons[i].addEventListener("click", app.callbacks.submitForm);
+		}
 	},
 
 	callbacks : {
@@ -18,6 +24,19 @@ var app = {
 			for (var i = 0; i<ids.length; i++){
 				app.dom.forms.showAForm(ids[i]);
 			}
+		},
+		submitForm : function(e){
+			e.preventDefault();
+			console.log(e.target.parentNode.getAttribute("id"));
+			formID = e.target.parentNode.getAttribute("id");
+			if (formID === "energy_form"){
+				app.dom.forms.submitEnergyForm();
+			}else if (formID === "pokemon_form"){
+				app.dom.forms.submitPokemonForm();
+			}else if (formID === "trainer_form"){
+				app.dom.forms.submitTrainerForm();
+			}
+			
 		},
 	},
 
@@ -32,7 +51,7 @@ var app = {
 			}
 	  	},
 		radioSelectorID : ".form_selector",
-
+		submitButtons : "form button",
 	    ids : ["#energy_form", "#pokemon_form", "#trainer_form"],
 		hideAll : function(){
 			this.ids.forEach(function(id){
@@ -43,6 +62,15 @@ var app = {
 			this.ids.forEach(function(id){
 				document.querySelector(id).style.display = "";
 			});
+		},
+		submitEnergyForm : function(){
+			console.log("energy form");
+		},
+		submitPokemonForm : function(){
+			console.log("pokemone form");
+		},
+		submitTrainerForm : function(){
+			console.log("trainer form");
 		},
 	  },
 		
